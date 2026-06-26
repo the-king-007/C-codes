8938b073bcba49f15fb3ba9d0d2f9c88ae149dc8
THE_KING <pranaykrs007@gmail.com>
2026-06-26 17:25:55 +0530

    adding files

---
+++ 13.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray (int arr[], int sz) {
+    int start = 0, end = sz -1;
+
+    while (start < end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 4, 5,7, 10, 48, 72};
+    int sz = 8;
+
+    reverseArray(arr, sz);
+
+    for (int i = 0; i < sz; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
